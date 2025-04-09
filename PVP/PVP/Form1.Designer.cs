c61fe397ab45ab251fce8232f46950468da9d88a
Marvin <162640870+MarvinTL-24@users.noreply.github.com>
2025-04-09 15:25:31 -0300

    OK

--- PVP/PVP/Form1.Designer.cs
+++ PVP/PVP/Form1.Designer.cs
@@ -46,8 +46,10 @@
             // TXT_TITULO
             // 
             this.TXT_TITULO.AutoSize = true;
+            this.TXT_TITULO.BackColor = System.Drawing.Color.Transparent;
             this.TXT_TITULO.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TXT_TITULO.Location = new System.Drawing.Point(166, 18);
+            this.TXT_TITULO.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.TXT_TITULO.Location = new System.Drawing.Point(167, 9);
             this.TXT_TITULO.Name = "TXT_TITULO";
             this.TXT_TITULO.Size = new System.Drawing.Size(322, 33);
             this.TXT_TITULO.TabIndex = 0;
@@ -55,9 +57,14 @@
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(83, 126);
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(101, 126);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(126, 24);
             this.label1.TabIndex = 1;
@@ -65,9 +72,14 @@
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(390, 126);
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(408, 126);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(126, 24);
             this.label2.TabIndex = 2;
@@ -75,11 +87,14 @@
             // 
             // BTN_ATQJ1
             // 
+            this.BTN_ATQJ1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.BTN_ATQJ1.Font = new System.Drawing.Font("News706 BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BTN_ATQJ1.ForeColor = System.Drawing.Color.Crimson;
-            this.BTN_ATQJ1.Location = new System.Drawing.Point(169, 216);
+            this.BTN_ATQJ1.Location = new System.Drawing.Point(187, 216);
             this.BTN_ATQJ1.Name = "BTN_ATQJ1";
-            this.BTN_ATQJ1.Size = new System.Drawing.Size(123, 43);
+            this.BTN_ATQJ1.Size = new System.Drawing.Size(124, 64);
             this.BTN_ATQJ1.TabIndex = 3;
             this.BTN_ATQJ1.Text = "ATAQUE";
             this.BTN_ATQJ1.UseVisualStyleBackColor = true;
@@ -87,22 +102,28 @@
             // 
             // BTN_DFJ1
             // 
+            this.BTN_DFJ1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.BTN_DFJ1.Font = new System.Drawing.Font("News706 BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BTN_DFJ1.ForeColor = System.Drawing.Color.Orange;
-            this.BTN_DFJ1.Location = new System.Drawing.Point(169, 352);
+            this.BTN_DFJ1.Location = new System.Drawing.Point(187, 352);
             this.BTN_DFJ1.Name = "BTN_DFJ1";
-            this.BTN_DFJ1.Size = new System.Drawing.Size(123, 43);
+            this.BTN_DFJ1.Size = new System.Drawing.Size(124, 64);
             this.BTN_DFJ1.TabIndex = 4;
             this.BTN_DFJ1.Text = "DEFESA";
             this.BTN_DFJ1.UseVisualStyleBackColor = true;
             // 
             // BTN_ATQJ2
             // 
+            this.BTN_ATQJ2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.BTN_ATQJ2.Font = new System.Drawing.Font("News706 BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BTN_ATQJ2.ForeColor = System.Drawing.Color.Crimson;
-            this.BTN_ATQJ2.Location = new System.Drawing.Point(476, 216);
+            this.BTN_ATQJ2.Location = new System.Drawing.Point(494, 216);
             this.BTN_ATQJ2.Name = "BTN_ATQJ2";
-            this.BTN_ATQJ2.Size = new System.Drawing.Size(123, 43);
+            this.BTN_ATQJ2.Size = new System.Drawing.Size(124, 64);
             this.BTN_ATQJ2.TabIndex = 5;
             this.BTN_ATQJ2.Text = "ATAQUE";
             this.BTN_ATQJ2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -111,55 +132,72 @@
             // 
             // BTN_DFJ2
             // 
+            this.BTN_DFJ2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.BTN_DFJ2.Font = new System.Drawing.Font("News706 BT", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BTN_DFJ2.ForeColor = System.Drawing.Color.Orange;
-            this.BTN_DFJ2.Location = new System.Drawing.Point(476, 351);
+            this.BTN_DFJ2.Location = new System.Drawing.Point(494, 351);
             this.BTN_DFJ2.Name = "BTN_DFJ2";
-            this.BTN_DFJ2.Size = new System.Drawing.Size(123, 43);
+            this.BTN_DFJ2.Size = new System.Drawing.Size(124, 64);
             this.BTN_DFJ2.TabIndex = 6;
             this.BTN_DFJ2.Text = "DEFESA";
             this.BTN_DFJ2.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(215, 123);
+            this.textBox1.Location = new System.Drawing.Point(233, 123);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(42, 29);
+            this.textBox1.Size = new System.Drawing.Size(43, 29);
             this.textBox1.TabIndex = 7;
             this.textBox1.Text = "100";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(522, 123);
+            this.textBox2.Location = new System.Drawing.Point(540, 123);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(42, 29);
+            this.textBox2.Size = new System.Drawing.Size(43, 29);
             this.textBox2.TabIndex = 8;
             this.textBox2.Text = "100";
             // 
             // pictureBox3
             // 
-            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox3.Enabled = false;
             this.pictureBox3.Image = global::PVP.Properties.Resources.pngwing_com__7_;
-            this.pictureBox3.Location = new System.Drawing.Point(314, 190);
+            this.pictureBox3.Location = new System.Drawing.Point(332, 190);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(156, 248);
+            this.pictureBox3.Size = new System.Drawing.Size(157, 269);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 11;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Enabled = false;
             this.pictureBox1.Image = global::PVP.Properties.Resources.pngwing_com__6_;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 190);
+            this.pictureBox1.Location = new System.Drawing.Point(26, 190);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(151, 248);
+            this.pictureBox1.Size = new System.Drawing.Size(152, 268);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
@@ -169,7 +207,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Teal;
-            this.ClientSize = new System.Drawing.Size(649, 450);
+            this.BackgroundImage = global::PVP.Properties.Resources.BLACKGROUNDLIMPO;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(646, 470);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox2);
@@ -181,6 +221,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.TXT_TITULO);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
             this.Text = "P1 X P2";
--- PVP/PVP/Properties/Resources.Designer.cs
+++ PVP/PVP/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace PVP.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap BLACKGROUNDLIMPO {
+            get {
+                object obj = ResourceManager.GetObject("BLACKGROUNDLIMPO", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap pngwing_com__6_ {
             get {
                 object obj = ResourceManager.GetObject("pngwing.com (6)", resourceCulture);
